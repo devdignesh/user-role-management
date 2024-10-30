d7bfeb58ae53944d5547cbcfd2a75c3d401a87a3
devdignesh <dignesh.hirapara@gmail.com>
2024-10-30 21:52:09 +0530

    Fixes

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,10 +1,9 @@
 import axios from "axios";
 
-const API_URL = 'https://f2ed36a4mh.execute-api.ap-south-1.amazonaws.com/'
+const API_URL = "https://f2ed36a4mh.execute-api.ap-south-1.amazonaws.com/";
 
 export const fetchUserData = async () => {
   try {
- 
     const response = await axios.get(API_URL);
     return response.data;
   } catch (error) {
